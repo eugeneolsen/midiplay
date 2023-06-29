d4b6c9a50c3a9dbc54a7bac674f73157c3b2dd83
Eugene C. Olsen <eugene@olsenfamily.org>
2023-06-29 10:43:28 -0400

    Move path calculation to utility.hpp

--- utility.hpp
+++ utility.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
+#include <string>
 #include <cctype>
+
+#include <boost/filesystem.hpp>
+
+namespace fs = boost::filesystem;
+
 
 bool isNumeric(const char *str)
 {
@@ -15,4 +21,28 @@ bool isNumeric(const char *str)
     }
 
     return true;
+}
+
+
+std::string getFullPath(std::string fileName)
+{
+    fs::path home = getenv("HOME");
+    fs::path dir = ("/Music/midihymns");
+    std::string extension = ".mid";
+    if (fileName.length() < extension.length())
+    {
+      fileName += extension;
+    }
+
+    std::string filenameEnd = fileName.substr(fileName.length() - extension.length(), extension.length());
+    if (filenameEnd != extension)
+    {
+      fileName += extension;
+    }
+
+    fs::path file = (fileName);
+
+    fs::path path = home / dir / file;
+
+    return path.string();
 }

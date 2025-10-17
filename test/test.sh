6692cb80a2351f278357d1a6bb80f28482807873
eugeneolsen <eugene@olsenfamily.org>
2025-10-16 20:21:08 -0400

    feat: add automated user acceptance test script.

---
+++ test/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+../play -s simple
+sleep 2
+../play -s with_intro
+sleep 2
+../play -s with_intro -x1 -V
+sleep 2
+../play -s with_intro -p
+sleep 2
+../play -s with_intro -t160
+sleep 2
+../play -s dc_al_fine
+sleep 2
+../play -s dc_al_fine -n2
+sleep 2
+../play -s ritardando

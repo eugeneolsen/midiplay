79fa9253bb7541d621d06e20dbdf0d10e3286466
eugeneolsen <eugene@olsenfamily.org>
2025-12-15 22:06:02 -0500

    feat: add a default pause of a 32nd note  for D.C. if no pause between verses.

--- playback_orchestrator.cpp
+++ playback_orchestrator.cpp
@@ -12,6 +12,10 @@
 using cxxmidi::Event;
 using cxxmidi::player::PlayerSync;
 
+namespace {
+    static constexpr int THIRTY_SECOND_NOTE_DIVISOR = 32; // divisor for a 32nd note
+}
+
 namespace MidiPlay {
 
 PlaybackOrchestrator::PlaybackOrchestrator(PlayerSync& player,
@@ -154,6 +158,10 @@ void PlaybackOrchestrator::playVerses() {
             // Pause before playing al Fine
             if (pauseTicks.has_value()) {
                 std::this_thread::sleep_for(std::chrono::microseconds(pauseTicks.getTicks().value() * uSecPerTick));
+            } else {
+                uint16_t ppq = midiLoader_.getFile().TimeDivision();
+                uint16_t thirtySecondNoteDuration = ppq / THIRTY_SECOND_NOTE_DIVISOR;                          // 32nd note
+                std::this_thread::sleep_for(std::chrono::milliseconds(thirtySecondNoteDuration * uSecPerTick)); // 32nd note pause default
             }
 
             player_.Play();
